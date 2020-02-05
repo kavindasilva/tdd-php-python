867512e32ee3fd399e77f92ce2ba150421145562
kavindasilva <kavinda@vetstoria.com>
2020-02-05 18:00:39 +0530

    WIP code craker kata

---
+++ php/tests/unit/CodecrackerCest.php
@@ -0,0 +1,56 @@
+<?php 
+
+namespace App\Unit;
+use App\Codecracker;
+
+class CodecrackerCest
+{
+    private $crackerClass = null;
+    private $decryptAlphabet = [
+        'a' => '!',
+        'b' => ')',
+        'c' => '"',
+        'd' => '(',
+        'e' => '$',
+        'f' => '*',
+        'g' => '%',
+        'h' => '&',
+        'i' => '>',
+        'j' => '<',
+        'k' => '@',
+        'l' => 'a',
+        'm' => 'b',
+        'n' => 'c',
+        'o' => 'd',
+        'p' => 'e',
+        'q' => 'f',
+        'r' => 'g',
+        's' => 'h',
+        't' => 'i',
+        'u' => 'j',
+        'v' => 'k',
+        'w' => 'l',
+        'x' => 'm',
+        'y' => 'n',
+        'z' => 'o',
+    ];
+
+    public function _before(\UnitTester $I)
+    {
+        $this->dict_class = new Codecracker();
+    }
+
+    protected $stringPairs = [
+        "apple" => '!eea$'
+    ];
+
+    public function decryptList(\UnitTester $I){
+        foreach($this->stringPairs as $word => $enc_word){
+            $I->assertEquals( $word, $this->crackerClass->decrypt($enc_word) );
+        }
+    }
+
+    protected function decryptWord($decryptedWord){
+
+    }
+}
