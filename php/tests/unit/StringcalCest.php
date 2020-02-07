c8403883adb3309d5188e2592163cf44b064f85e
kavindasilva <kavinda@vetstoria.com>
2020-02-07 17:23:51 +0530

    configured stringcal class

--- php/tests/unit/StringcalCest.php
+++ php/tests/unit/StringcalCest.php
@@ -4,12 +4,13 @@ namespace App\Unit;
 class StringcalCest
 {
     protected $cal = null;
-    public function _before(UnitTester $I)
+    public function _before(\UnitTester $I)
     {
+        $this->cal = new \App\Stringcal();
     }
 
     // tests
-    public function checkEmptyString(UnitTester $I)
+    public function checkEmptyString(\UnitTester $I)
     {
         $result = $this->cal->add();
         $I->assertEquals("", $result, "checkEmptyString() failed");
