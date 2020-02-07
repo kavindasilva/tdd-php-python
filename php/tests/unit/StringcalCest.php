d11e4fa3f87cf8cb242b56b0628a808a218c99e5
kavindasilva <kavinda@vetstoria.com>
2020-02-07 17:58:26 +0530

    WIP test cases for any number of params accepting

--- php/tests/unit/StringcalCest.php
+++ php/tests/unit/StringcalCest.php
@@ -22,6 +22,8 @@ class StringcalCest
      * @example { "send": "2", "expect": "2" }
      * @example { "send": "2.5", "expect": "2.5" }
      * @example { "send": "106", "expect": "106" }
+     * 
+     * @example { "send": "", "expect": "0" }
      */
     public function checkSingleParamInput(\UnitTester $I, \Codeception\Example $Example)
     {
@@ -34,8 +36,22 @@ class StringcalCest
      * @example { "para1": "1.5", "para2": "60.4", "expect": "61.9" }
      * @example { "para1": "1.131", "para2": "4.099", "expect": "5.230" }
      * @example { "para1": "100", "para2": "0.5", "expect": "100.5" }
+     * 
+     * @example { "para1": "", "para2": "", "expect": "0" }
+     * @example { "para1": "2001", "para2": "", "expect": "2001" }
+     * @example { "para1": "", "para2": "0.5", "expect": "0" }
      */
     public function checkTwoParamInput(\UnitTester $I, \Codeception\Example $Example)
+    {
+        $result = $this->cal->add( $Example["para1"], $Example["para2"] );
+        $I->assertEquals( $Example["expect"], $result, "checkEmptyString() failed");
+    }
+
+    /**
+     * @example { "para1": "4", "para2": "5", "para3": "5", "expect": "14" }
+     * @example { "para1": "12.1", "para2": "5.77", "para3": "5.5", "expect": "23.37" }
+     */
+    public function checkAnyNumberOfParamInput(\UnitTester $I, \Codeception\Example $Example)
     {
         $result = $this->cal->add( $Example["para1"], $Example["para2"] );
         $I->assertEquals( $Example["expect"], $result, "checkEmptyString() failed");
