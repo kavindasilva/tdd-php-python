6d1c874530a2873230ff234de285468465ed6692
kavindasilva <kavinda@vetstoria.com>
2020-02-11 18:28:50 +0530

    WIP test name fixed

--- php/tests/unit/NumToWordCest.php
+++ php/tests/unit/NumToWordCest.php
@@ -1,17 +1,20 @@
 <?php 
 
 namespace App\Unit;
-class NumToWord
+class NumToWordCest
 {
     public function _before(\UnitTester $I)
     {
+        $this->class = new \App\NumToWord();
     }
 
     /**
-     * @example { "send": "745.00", "expect": "Seven Hundred and forty five" }
-     * @example0 { "send": "745.00", "expect": "2.5" }
+     * @example { "send": "745.00", "expect": "seven hundred and forty five" }
+     * example { "send": "745.00", "expect": "2.5" }
      */
     public function tryToTest(\UnitTester $I, \Codeception\Example $Example)
     {
+        $result = $this->class->convertNumber($Example["send"]);
+        $I->assertEquals( $Example["expect"], strtolower($result), "failed" );
     }
 }
