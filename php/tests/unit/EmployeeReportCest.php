7fd498406138591dbf1ea594e138d77a820fd9cb
kavindasilva <kavinda@vetstoria.com>
2020-02-28 17:09:02 +0530

    WIP test cases

--- php/tests/unit/EmployeeReportCest.php
+++ php/tests/unit/EmployeeReportCest.php
@@ -6,22 +6,22 @@ namespace App\Unit;
  */
 class EmployeeReportCest
 {
+    /**
+     * TODO :create stubs & mocks
+     */
+    const EMPLOYEES = [
+        [ "name" => 'Max', "age" => 17 ],
+        [ "name" => 'Sepp', "age" => 18 ],
+        [ "name" => 'Nina', "age" => 15 ],
+        [ "name" => 'Mike', "age" => 51 ],
+    ];
+
     public function _before(\UnitTester $I)
     {
         $this->class = new \App\EmployeeReport();
     }
 
-    /**
-     * @example { "send": "745.00", "expect": "seven hundred and forty five" }
-     * @example { "send": "1.00", "expect": "one" }
-     * @example { "send": "6.00", "expect": "six" }
-     * @example { "send": "10.00", "expect": "ten" }
-     * @example { "send": "11.00", "expect": "eleven" }
-     * @example { "send": "17.00", "expect": "seventeen" }
-     * @example { "send": "20.00", "expect": "twenty" }
-     * @example { "send": "140.00", "expect": "hundred and forty" }
-     * example { "send": "745.00", "expect": "2.5" }
-     */
+
     public function tryToTest(\UnitTester $I, \Codeception\Example $Example)
     {
         $result = $this->class->convertNumber($Example["send"]);
