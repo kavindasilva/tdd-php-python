72f6ef861f56e5e6e94e2a55870125caece090de
kavindasilva <kss.kavinda@gmail.com>
2020-08-02 18:23:22 +0530

    multiple elements

--- php/src/permutations.php
+++ php/src/permutations.php
@@ -10,6 +10,19 @@ class Permutations{
         if( count($elems) === 1 ){
             return [ $elems[0] ];
         }
+        if( count($elems) === 2 ){
+            return [
+                $elems[0].$elems[1],
+                $elems[1].$elems[0]
+            ];
+        }
+        $erst_of_arr = array_shift($elems);
+        return[
+            array_merge(
+                [ $elems[0] ],
+                $this->getPermutations( $elems )
+            )
+        ];
     }
 }
 
--- php/tests/unit/PermutationsCest.php
+++ php/tests/unit/PermutationsCest.php
@@ -3,6 +3,11 @@
 namespace App\Unit;
 use App\Permutations;
 
+/**
+ * array equality variations taken from: https://stackoverflow.com/questions/5678959/php-check-if-two-arrays-are-equal
+ */
+
+
 class PermutationsCest
 {
     public function _before(\UnitTester $I)
@@ -14,12 +19,26 @@ class PermutationsCest
      * @example { "arr": [], "expect": false }
      * @example { "arr": ["A"], "expect": ["A"] }
      * @example { "arr": ["A", "B"], "expect": ["AB", "BA"] }
+     * @example { "arr": ["A", "B", "C"], "expect": [ "ABC", "ACB", "BAC", "BCA", "CAB", "CBA"] }
      * example { "arr": 0.5, "expect": false }
      */
     public function checkPermutations(\UnitTester $I, \Codeception\Example $Example)
     {
         $result = $this->class->getPermutations($Example["arr"]);
         $I->assertEquals( $Example["expect"], $result, "failed" );
+    } 
+
+    // array_diff($a, $b) === array_diff($b, $a)
+    /**
+     * @example { "arr": ["A", "B"], "expect": ["AB", "BA"] }
+     * @example { "arr": ["A", "B"], "expect": ["BA", "AB"] }
+     */
+    public function checkPermutationsWithoutOrder(\UnitTester $I, \Codeception\Example $Example)
+    {
+        $result = $this->class->getPermutations($Example["arr"]);
+        $res1 = array_diff($Example["expect"], $result);
+        $res2 = array_diff($result, $Example["expect"]);
+        $I->assertEquals( $res1, $res2, "failed" );
     } 
 
 }
