5eaa08f4d8ddc429f3063ef22bb2d36b89adb93d
kavindasilva <kss.kavinda@gmail.com>
2020-08-04 22:24:51 +0530

    more tests

--- php/tests/unit/PermutationsCest.php
+++ php/tests/unit/PermutationsCest.php
@@ -17,12 +17,28 @@ class PermutationsCest
 
     /**
      * @example { "arr": [], "expect": false }
+     * @example { "arr": "", "expect": false }
+     * @example { "arr": 1, "expect": false }
+     * @example { "arr": "string", "expect": false }
+     * @example { "arr": null, "expect": false }
+     * @example { "arr": true, "expect": false }
      * @example { "arr": ["A"], "expect": ["A"] }
      * @example { "arr": ["A", "B"], "expect": ["AB", "BA"] }
      * @example { "arr": ["A", "B", "C"], "expect": [ "ABC", "ACB", "BAC", "BCA", "CAB", "CBA"] }
-     * example { "arr": 0.5, "expect": false }
      */
     public function checkPermutations(\UnitTester $I, \Codeception\Example $Example)
+    {
+        $result = $this->class->getPermutations($Example["arr"]);
+        $I->assertEquals( $Example["expect"], $result, "failed" );
+    } 
+
+
+    /**
+     * @example { "arr": ["A"], "expect": ["A"] }
+     * @example { "arr": ["A", "B"], "expect": ["AB", "BA"] }
+     * @example { "arr": ["A", "B", "C"], "expect": [ "ABC", "ACB", "BAC", "BCA", "CAB", "CBA"] }
+     */
+    public function calculatePermutations(\UnitTester $I, \Codeception\Example $Example)
     {
         $result = $this->class->calculatePermutations($Example["arr"]);
         $I->assertEquals( $Example["expect"], $result, "failed" );
@@ -33,7 +49,7 @@ class PermutationsCest
      * @example { "arr": ["A", "B"], "expect": ["BA", "AB"] }
      * @example { "arr": ["A", "B", "C", "D"], "expect": [ "ABCD", "ACBD", "BACD", "BCAD", "CABD", "CBAD",  "ABDC", "ACDB", "BADC", "BCDA", "CADB", "CBDA",  "ADBC", "ADCB", "BDAC", "BDCA", "CDAB", "CDBA",  "DABC", "DACB", "DBAC", "DBCA", "DCAB", "DCBA"] }
      */
-    public function checkPermutationsWithoutOrder(\UnitTester $I, \Codeception\Example $Example)
+    public function calculatePermutationsWithoutOrder(\UnitTester $I, \Codeception\Example $Example)
     {
         $result = $this->class->calculatePermutations($Example["arr"]);
         $diff = ( array_diff($Example["expect"], $result) === array_diff($result, $Example["expect"]) ) ? true : false;
